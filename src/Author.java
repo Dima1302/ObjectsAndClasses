4557348d551141f9737af74c28014ee8c37b3a9f
Dmitriy <Dimitriy303@yandex.ru>
2022-06-09 00:09:04 +0900

    Initial commit

---
+++ src/Author.java
@@ -0,0 +1,18 @@
+public class Author {
+    String name;
+    String lastName;
+
+    public Author(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
+
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Author author1 = new Author("Джордж","Оруэлл");
+        Author author2 = new Author("Олдос","Хаксли");
+Book book1 = new Book("1986",author1,1949);
+Book book2 = new Book("О дивный мир",author2,1932);
+
+    }
+}
